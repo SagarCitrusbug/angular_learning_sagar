52643873077fcb23bc24fd0e3ae48e1b76fe0d0b
SagarCitrusbug <sagar.citrusbug@gmail.com>
2022-01-25 18:26:10 +0530

    How to install Material UI.

--- Angular/concept/src/app/app.module.ts
+++ Angular/concept/src/app/app.module.ts
@@ -5,6 +5,7 @@ import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -14,9 +15,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     FormsModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+// install bootstrap in project using below commend
+// ng add @ng-bootstrap/ng-bootstrap
+
+// install material ui in project using below commend
+// ng add @angular/material
